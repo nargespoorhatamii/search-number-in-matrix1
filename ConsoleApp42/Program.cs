fd88e34a97d3ff383b4cdee57e6e2e9166da4782
nargespoorhatamii <126357653+nargespoorhatamii@users.noreply.github.com>
2023-06-18 13:48:37 +0430

    Add project files.

---
+++ ConsoleApp42/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Proggram
+{
+    class test
+    {
+        public static void Main()
+        {
+            int[,] matrix = new int[3, 3];
+
+            int counter = 0; //counter for count
+            Console.WriteLine("inter nine number");
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int j = 0; j <= 2; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            } //you need to describe matrix twice 
+            Console.WriteLine("enter the number");
+            int number = Convert.ToInt32(Console.ReadLine()); //after call a first matrix code you need to conver
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int j = 0; j <= 2; j++)
+                {
+                    if (matrix[i, j] == number)
+                    {
+                        counter++;
+                    }
+
+                }
+            }
+            Console.WriteLine(counter);
+
+        }
+    }
+
+}
